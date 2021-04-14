b3e8fc9d6fae74b3015f92bd35a12003ce95924f
Suraj vishwakarma <vishwakarmasuraj631@gmail.com>
2021-04-14 20:32:01 +0530

    WEBSITE COMPLETED

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 let addBtn = document.getElementById("addBtn");
 
-addBtn.addEventListener("click", function(e) {
+showNotes();
+
+addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
-    } 
+    }
     else {
         notesObj = JSON.parse(notes);
     }
     notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
-    console.log(notesObj);
+    // console.log(notesObj);
     showNotes();
 })
 
@@ -21,26 +23,70 @@ function showNotes() {
     let notes = localStorage.getItem('notes');
 
     if (notes == null) {
-      notesObj = [];
-    } 
+        notesObj = [];
+    }
     else {
-      notesObj = JSON.parse(notes);
+        notesObj = JSON.parse(notes);
     }
 
     let html = "";
 
-    notesObj.forEach(function(element, index) {
-      html += `<div class="notescard my-2 mx-2 card" style="width: 18rem;">
+    notesObj.forEach(function (element, index) {
+        html += `<div class="notescard my-2 mx-2 card" style="width: 18rem;">
                       <div class="card-body">
                           <h5 class="card-title">Note ${index + 1}</h5>
                           <p class="card-text"> ${element}</p>
-                          <button class="btn btn-primary">Delete Note</button>
+                          <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                       </div>
                   </div>`;
     });
 
     let notesElm = document.getElementById("notes");
-    if (notes.length != 0) {
-      notesElm.innerHTML = html;
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
     }
-  }
+    else {
+        notesElm.innerHTML = `<h5> No Notes Added, Use Above Section To Add Notes. </h5>`
+    }
+}
+
+// Function to delete elements from localStorage
+function deleteNote(index) {
+    // console.log('I am Deleting', index);
+
+    let notes = localStorage.getItem('notes');
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+
+    showNotes();
+
+}
+
+//Function to search
+let searchTxt = document.getElementById("searchTxt");
+
+searchTxt.addEventListener('input', function () {
+
+    let inputVal = searchTxt.value;
+    // console.log('Input event Fired !!!', inputVal);
+    let noteCards = document.getElementsByClassName("notescard");
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+    })
+})
+
